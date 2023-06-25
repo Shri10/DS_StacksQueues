dac03d7b61a6640b24188d7634d45000be7f77f8
Shri10 <pethe.shriraj@gmail.com>
2023-06-25 21:15:44 +0530

    [Shri10] Created UC1

--- DS_StacksQueues/Program.cs
+++ DS_StacksQueues/Program.cs
@@ -5,6 +5,32 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Data Structures, Demonstration and Implmentation of Stacks and Queues!");
+
+            LinkedList<int> stack = new LinkedList<int>();
+
+            Push(stack, 70);
+            Push(stack, 30);
+            Push(stack, 56);
+
+            Display(stack);
+
         }
+
+        static void Push(LinkedList<int> stack, int data)
+        {
+            stack.AddFirst(data);
+        }
+
+        static void Display(LinkedList<int> stack)
+        {
+            var node = stack.First;
+
+            while (node != null)
+            {
+                Console.WriteLine(node.Value);
+                node = node.Next;
+            }
+        }
+
     }
 }
