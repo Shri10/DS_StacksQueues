c9addd0fb9c76fdd5f5e17f311db65f953ce81a8
Shri10 <pethe.shriraj@gmail.com>
2023-06-25 21:21:47 +0530

    [Shri10] Created UC2

--- DS_StacksQueues/Program.cs
+++ DS_StacksQueues/Program.cs
@@ -4,7 +4,7 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to Data Structures, Demonstration and Implmentation of Stacks and Queues!");
+            Console.WriteLine("Welcome to Data Structures, Demonstration and Implmentation of Stacks and Queues!\n");
 
             LinkedList<int> stack = new LinkedList<int>();
 
@@ -12,13 +12,34 @@
             Push(stack, 30);
             Push(stack, 56);
 
+            Console.WriteLine();
             Display(stack);
+            Console.WriteLine();
 
+            while (stack.Count > 0)
+            {
+                Console.WriteLine($"Top element is {Peek(stack)}");
+                Console.WriteLine($"Popped element is {Pop(stack)}");
+                Console.WriteLine($"Stack size is {stack.Count}");
+                Console.WriteLine();
+            }
         }
 
         static void Push(LinkedList<int> stack, int data)
         {
             stack.AddFirst(data);
+        }
+
+        static int Pop(LinkedList<int> stack)
+        {
+            int poppedValue = stack.First.Value;
+            stack.RemoveFirst();
+            return poppedValue;
+        }
+
+        static int Peek(LinkedList<int> stack)
+        {
+            return stack.First.Value;
         }
 
         static void Display(LinkedList<int> stack)
@@ -31,6 +52,5 @@
                 node = node.Next;
             }
         }
-
     }
 }
